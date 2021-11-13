029bec012102f026fe7c4a126d1cdc057679a7e1
Gabriel Dechichi <gabrieldechichi@gmail.com>
2021-11-12 20:18:09 -0800

    221 - Desafio: Field of View

--- 221_CampoDeVisao/Assets/Scripts/FieldOfView.cs
+++ 221_CampoDeVisao/Assets/Scripts/FieldOfView.cs
@@ -12,11 +12,13 @@ public class FieldOfView : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        var isVisible = FieldOfViewUtils.IsInsideFieldOfView(transform.position, target.position, viewAngle);
+        var isVisible = FieldOfViewUtils.IsInsideFieldOfView(transform.position, target.position, viewDistance, viewAngle);
 
         Gizmos.color = isVisible ? Color.green : Color.red;
         Gizmos.DrawWireSphere(transform.position, viewDistance);
-        Gizmos.DrawLine(transform.position, transform.position + Quaternion.Euler(0, 0, viewAngle * 0.5f) * Vector3.up * viewDistance);
-        Gizmos.DrawLine(transform.position, transform.position + Quaternion.Euler(0, 0, -viewAngle * 0.5f) * Vector3.up * viewDistance);
+        var xDelta = viewDistance * Mathf.Sin(Mathf.Deg2Rad * viewAngle * 0.5f);
+        var yDelta = viewDistance * Mathf.Cos(Mathf.Deg2Rad * viewAngle * 0.5f);
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(xDelta, yDelta, 0));
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(-xDelta, yDelta, 0));
     }
 }
