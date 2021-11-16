c4f18fc86c5bf274153cbde55876eeb02e32ee23
Gabriel Dechichi <gabrieldechichi@gmail.com>
2021-11-15 16:39:16 -0800

    221 - FOV 2D fixes

--- 221_CampoDeVisao/Assets/Scripts/FieldOfViewUtils.cs
+++ 221_CampoDeVisao/Assets/Scripts/FieldOfViewUtils.cs
@@ -4,15 +4,25 @@ public static class FieldOfViewUtils
 {
     public static bool IsInsideFieldOfView(Vector2 fovOrigin, Vector2 target, float viewDistance, float viewAngle)
     {
-        var sqrDist = DistanceBetweenToPoints(fovOrigin, target);
-        if (sqrDist > viewDistance)
+        //fovOrigin = posicao do inimigo
+        //target = posicao do player
+
+        if (target.y < fovOrigin.y)
         {
             return false;
         }
-        var frontPoint = new Vector2(fovOrigin.x, target.y);
-        var cat = DistanceBetweenToPoints(frontPoint, target);
 
-        var sinAngle = cat / sqrDist;
+        var dist = DistanceBetweenToPoints(fovOrigin, target);
+        if (dist > viewDistance)
+        {
+            return false;
+        }
+
+        var cat = Mathf.Abs(target.x - fovOrigin.x);
+        var sinAngle = cat / dist;//seno de teta
+                                  // var teta = Mathf.Asin(sinAngle);
+                                  // return teta <= viewAngle * 0.5f;
+
         var sinHalfViewAngle = Mathf.Sin(Mathf.Deg2Rad * viewAngle * 0.5f);
 
         return sinAngle <= sinHalfViewAngle;
